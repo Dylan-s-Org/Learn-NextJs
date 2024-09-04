425e948c750d07999a5d5b7147dac7c5d7b1b9c6
MinHee <dylandang.dev@gmail.com>
2024-09-04 23:45:34 +0700

    dev: Validating Requests with Zod

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
 
 export function GET(
   request: NextRequest,
@@ -16,8 +17,9 @@ export async function PUT(
   { params }: { params: { id: number } }
 ) {
   const body = await request.json();
-  if (!body.name)
-    return NextResponse.json({ error: "Name is required" }, { status: 404 });
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 404 });
 
   if (params.id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
