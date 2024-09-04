78e7dc7e58433fb430e2890e442f475f8224ceda
MinHee <dylandang.dev@gmail.com>
2024-09-04 23:10:30 +0700

    dev: Update an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,9 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-interface Props {
-  params: { id: number };
-}
-
 export function GET(
   request: NextRequest,
   { params }: { params: { id: number } }
@@ -13,4 +9,18 @@ export function GET(
   }
 
   return NextResponse.json({ id: 1, name: "minh" });
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 404 });
+
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: body.name });
 }
