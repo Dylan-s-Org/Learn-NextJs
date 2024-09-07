5dc75d7d0b6faaf224549bd33eae6c134f7d8a6b
MinHee <dylandang.dev@gmail.com>
2024-09-07 17:46:37 +0700

    dev: Getting Data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,12 +1,11 @@
 import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "minh" },
-    { id: 2, name: "huy" },
-  ]);
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
