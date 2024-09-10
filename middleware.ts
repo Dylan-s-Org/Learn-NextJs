19e45d9d4cc94c8c90f244ecf0cf9e0c095fa1a0
MinHee <dylandang.dev@gmail.com>
2024-09-10 23:26:28 +0700

    dev: Protecting Routes

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { NextRequest } from "next/server";
+export { default } from "next-auth/middleware";
+
+export const config = {
+  // *: zero or more
+  // +: one or more
+  // ?: zero or more
+  matcher: ["/dashboard/:path*"],
+};
