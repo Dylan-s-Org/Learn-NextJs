d3848535c7eba5ebf576f7cd57816815febcd20b
Dylan Dang <55476813+minhhuy11022000@users.noreply.github.com>
2024-09-14 00:34:41 +0700

    Lazy Loading
    * dev: Using Third-party Scripts
    
    * dev: Optimizing Images
    
    * dev: Using Fonts
    
    * dev: Lazy Loading 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,20 @@
-import Image from "next/image";
-import ProductCard from "./components/ProductCard";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/authOptions";
-import coffee from "@/public/images/coffee.jpg";
-import { Metadata } from "next";
+"use client";
+import { useState } from "react";
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
-
+export default function Home() {
   return (
     <main className="relative h-screen">
-      <Image
-        src={"https://bit.ly/react-cover"}
-        alt="coffee"
-        fill
-        className="object-cover"
-        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
-        quality={100}
-        priority
-      />
+      <h1>Hello World</h1>
+      <button
+        onClick={async () => {
+          const _ = (await import("lodash")).default;
+          const users = [{ name: "c" }, { name: "b" }, { name: "a" }];
+
+          const sortedArr = _.orderBy(users, ["name"]);
+        }}
+      >
+        Show
+      </button>
     </main>
   );
 }
