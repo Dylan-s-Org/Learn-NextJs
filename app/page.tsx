420f446a670fa00585025cf7e45f7c4fb4cb1d97
MinHee <dylandang.dev@gmail.com>
2024-09-13 23:59:34 +0700

    dev: Lazy Loading

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/authOptions";
 import coffee from "@/public/images/coffee.jpg";
+import { Metadata } from "next";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -21,3 +22,16 @@ export default async function Home() {
     </main>
   );
 }
+
+// export const metadata: Metadata = {
+//   title: "Home"
+// }
+
+// export async function generateMetadata(): Promise<Metadata> {
+//   const product = await fetch("");
+
+//   return {
+//     title: "product.title",
+//     description: "...",
+//   };
+// }
